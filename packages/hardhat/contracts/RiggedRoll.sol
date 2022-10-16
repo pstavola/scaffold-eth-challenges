02f64499bf9b62e4f6c4dc96f21847674e3af93a
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-10-16 11:25:28 +0200

    checkpoint 4: withdraw

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -14,6 +14,10 @@ contract RiggedRoll is Ownable {
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
+    function withdraw(address _addr, uint256 _amount) public onlyOwner {
+        (bool sent, ) = payable(_addr).call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+    }
 
 
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
