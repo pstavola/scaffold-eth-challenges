b74ea8caedde9d123fa9425fda3a3c8bcda26128
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-10-16 11:43:04 +0200

    deploy to goerli

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -28,7 +28,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 //
 // Select the network you want to deploy to here:
 //
-const defaultNetwork = "localhost";
+const defaultNetwork = "goerli";
 
 const mainnetGwei = 21;
 
@@ -139,7 +139,7 @@ module.exports = {
       },
     },
     goerli: {
-      url: "https://goerli.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://goerli.infura.io/v3/1fc7c7c3701c4083b769e561ae251f9a", // <---- YOUR INFURA ID! (or it won't work)
 
       //      url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXXXX/eth/goerli", // <---- YOUR MORALIS ID! (not limited to infura)
 
