a2f4af1a21713d2e2cf679f143c2e479af59f02b
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-10-20 18:22:48 +0200

    more descriptive variable names and error messages; added some require checks to addSigner, RemoveSigner and updateSignaturesRequired; added validSignatures counter

--- packages/hardhat/contracts/MultiSigWallet.sol
+++ packages/hardhat/contracts/MultiSigWallet.sol
@@ -11,7 +11,7 @@ contract MultiSigWallet {
 
   mapping (address => bool) public isOwner;
   mapping (address => bool) public hasSigned;
-  uint256 public sigsRequired;
+  uint256 public signaturesRequired;
   uint256 public nonce = 0;
   string public purpose = "Building Unstoppable Apps!!!";
 
@@ -27,9 +27,9 @@ contract MultiSigWallet {
     _;
   }
 
-  constructor(address[] memory _owners, uint256 _approvalsRequired) payable {
+  constructor(address[] memory _owners, uint256 _sigsRequired) payable {
     require(_owners.length > 0, "MetaMultiSigWallet must have at least 1 owner");
-    require(_approvalsRequired <= _owners.length, "number of approvals must be less or equal to number of owners");
+    require(_sigsRequired <= _owners.length, "number of approvals must be less or equal to number of owners");
 
     for(uint256 i; i < _owners.length; i++){
       require(_owners[i] != address(0), "owner cannot be address(0)");
@@ -37,26 +37,28 @@ contract MultiSigWallet {
 
       isOwner[_owners[i]] = true;
     }
-    sigsRequired = _approvalsRequired;
+    signaturesRequired = _sigsRequired;
   }
 
   function getHash(uint256 _nonce, address to, uint256 value) public view returns (bytes32) {
-    return keccak256(abi.encodePacked(address(this),_nonce,to,value));
+    return keccak256(abi.encodePacked(address(this), purpose, _nonce, to, value));
   }
 
-  function executeTransaction(address payable _to, uint256 _value, bytes[] memory _signatures) public {
-    require(_signatures.length >= sigsRequired, "not enough approvals");
-
+  function executeTransaction(address payable _to, uint256 _value, bytes[] memory _signatures) public onlyOwner {
     bytes32 hash = getHash(nonce, _to, _value);
     address[] memory listOfSigners;
+    uint256 validSignatures;
     
     for(uint256 i; i < _signatures.length; i++){
       address signer = recover(hash, _signatures[i]);
-      require(isOwner[signer], "Signer must be one of the Owners");
-      require(!hasSigned[signer], "Signed more than once by the same owner");
+      require(isOwner[signer], "One of the signers is not an Owners");
+      require(!hasSigned[signer], "One of the signers signed more than once");
       hasSigned[signer] = true;
       listOfSigners[i] = signer;
+      validSignatures++;
     }
+
+    require(validSignatures >= signaturesRequired, "Not enough valid signatures");
     
     nonce++;
     ( bool success, ) = _to.call{value: _value}("");
@@ -77,15 +79,24 @@ contract MultiSigWallet {
     emit SetPurpose(msg.sender, purpose);
   }
 
-  function addSigner(address _newSigner) public onlyOwner {
+  function addSigner(address _newSigner, uint256 _sigsReq) public onlyOwner {
+    require(_newSigner != address(0), "New signer cannot be address(0)");
+    require(!isOwner[_newSigner], "Signer already exists");
     isOwner[_newSigner] = true;
+    updateSignaturesRequried(_sigsReq);
   }
 
-  function removeSigner(address _oldSigner) public onlyOwner {
+  function removeSigner(address _oldSigner, uint256 _sigsReq) public onlyOwner {
+    require(isOwner[_oldSigner], "Signer you want to remove is not an owner");
     isOwner[_oldSigner] = false;
+    updateSignaturesRequried(_sigsReq);
   }
 
   function updateSignaturesRequried(uint256 _sigsReq) public onlyOwner {
-    sigsRequired = _sigsReq;
+    require(_sigsReq > 0, "Signatures required cannot be 0.");
+    signaturesRequired = _sigsReq;
   }
+
+  receive() external payable {}
+  fallback() external payable {}
 }
